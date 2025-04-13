c6c94243371d2b61598d057913fcca2708c868ae
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-14 03:42:24 +0800

    refactor: test outcome

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => ({
     analog({
       nitro: {
         prerender: {
-          ignore: ['/'],
+          ignore: ['/index.html'],
         },
       },
     }),
